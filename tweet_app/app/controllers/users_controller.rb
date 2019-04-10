0fa6dc48de5cce5508337cc32463c24915d29759
rururu16 <ynk618172@gmail.com>
2019-04-10 15:58:34 +0900

    add

--- tweet_app/app/controllers/users_controller.rb
+++ tweet_app/app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
   before_action :ensure_correct_user, {only: [:edit, :update]}
